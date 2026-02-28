c0fa2133016bfeacd663013dd7d1a7a1b2179c0b
vaniadep <149788717+vaniadep@users.noreply.github.com>
2026-02-28 18:22:02 +0300

    Delete todoapp directory

--- todoapp/src/data/initialTodos.ts
+++
@@ -1,7 +0,0 @@
-import type { Todo } from "../types/todo";
-
-export const initialTodos: Todo[] = [
-  { id: 1, text: "Изучить TypeScript", completed: false },
-  { id: 2, text: "Сделать todo-приложение", completed: true },
-  { id: 3, text: "Добавить стили и анимации", completed: false },
-];
